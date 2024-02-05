a507616bc1a54745b16f81ef60c94ac312c467df
gorohov <gorohov@krista.ru>
2024-02-05 20:07:57 +0300

    Добавляет возможность работы с переменными окружения

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
+import { RestConfig } from './common/libs/config/rest.config.js';
 import { Logger } from './common/libs/logger/logger.js';
 import { RestApplication } from './rest/rest.application.js';
 
 
 async function bootstrap() {
   const logger = new Logger();
-  const application = new RestApplication(logger);
+  const config = new RestConfig(logger);
+  const application = new RestApplication(logger, config);
 
   await application.init();
 }
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,16 @@
-import { Logger } from '../common/libs/logger/logger.js';
+import { IConfig } from '../common/libs/config/config.interface.js';
+import { RestSchema } from '../common/libs/config/rest.schema.js';
+import { ILogger } from '../common/libs/logger/logger.interface.js';
 
 
 export class RestApplication {
   constructor (
-    private readonly logger: Logger
+    private readonly logger: ILogger,
+    private readonly config: IConfig<RestSchema>
   ) {}
 
   public async init () {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
